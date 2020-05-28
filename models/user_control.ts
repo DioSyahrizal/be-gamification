76034a6719850815049c1761d2e12fe7c657226b
diosyahrizal <diosyahrizal@gmail.com>
2020-05-28 20:42:14 +0700

    feat(admin): add router user controller

---
+++ models/user_control.ts
@@ -0,0 +1,52 @@
+import Sequelize, { Model } from "sequelize";
+import { sequelize } from "../utils/db";
+
+export class UserControl extends Model {
+  public id!: string;
+  public username!: string;
+  public name!: string;
+  public email!: string;
+  public address!: string;
+  public point!: number;
+  public fis_med!: string;
+  public fis_hard!: string;
+  public kim_med!: string;
+  public kim_hard!: string;
+}
+
+UserControl.init(
+  {
+    id: {
+      type: Sequelize.STRING,
+      primaryKey: true,
+    },
+    username: Sequelize.STRING,
+    name: Sequelize.STRING,
+    email: Sequelize.STRING,
+    address: Sequelize.TEXT,
+    point: Sequelize.BIGINT,
+    fis_med: {
+      type: Sequelize.ENUM,
+      values: ["lock", "open"],
+    },
+    fis_hard: {
+      type: Sequelize.ENUM,
+      values: ["lock", "open"],
+    },
+    kim_med: {
+      type: Sequelize.ENUM,
+      values: ["lock", "open"],
+    },
+    kim_hard: {
+      type: Sequelize.ENUM,
+      values: ["lock", "open"],
+    },
+  },
+  { sequelize, tableName: "users", timestamps: false }
+);
+
+// User.belongsToMany(Soal, {
+//   through: UserSoal,
+//   foreignKey: "id_user",
+//   otherKey: "id_soal"
+// });
--- router/control.router.ts
+++ router/control.router.ts
@@ -2,6 +2,8 @@ import { Router, Request, Response } from "express";
 import { Op } from "sequelize";
 import { User } from "../models/users";
 import { UserSoal } from "../models/users_soal";
+import { Menu } from "../models/menu";
+import { UserControl } from "../models/user_control";
 
 export const controlRouter = Router();
 
@@ -20,7 +22,7 @@ controlRouter.get("/count", async (_req: Request, res: Response) => {
 });
 
 controlRouter.get("/user", async (_req: Request, res: Response) => {
-  User.findAll({ attributes: { exclude: ["password", "role"] } }).then(
+  UserControl.findAll({ attributes: { exclude: ["password", "role"] } }).then(
     (user) => {
       res.status(200).json(user);
     }
@@ -28,10 +30,85 @@ controlRouter.get("/user", async (_req: Request, res: Response) => {
 });
 
 controlRouter.put("/user", async (req: Request, res: Response) => {
-  const { id, username, name, email, address } = req.body;
-  User.update({ username, name, email, address }, { where: { id: id } }).then(
-    (_user) => {
-      res.status(200).json({ status: "success" });
+  const { id, username, name, email, address, point } = req.body;
+  UserControl.update(
+    {
+      username: username,
+      name: name,
+      email: email,
+      address: address,
+      point: point,
+    },
+    { where: { id: id } }
+  ).then((_user) => {
+    res.status(200).json({ status: "success" });
+  });
+});
+
+controlRouter.put("/fis_med/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  Menu.update(
+    { fis_med: "open" },
+    {
+      where: {
+        id: id,
+      },
+      returning: true,
     }
+  ).then((_menu) =>
+    Menu.findOne({ where: { id: id } }).then((data) =>
+      res.status(200).json(data)
+    )
+  );
+});
+
+controlRouter.put("/fis_hard/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  Menu.update(
+    { fis_hard: "open" },
+    {
+      where: {
+        id: id,
+      },
+      returning: true,
+    }
+  ).then((_menu) =>
+    Menu.findOne({ where: { id: id } }).then((data) =>
+      res.status(200).json(data)
+    )
+  );
+});
+
+controlRouter.put("/kim_med/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  Menu.update(
+    { kim_med: "open" },
+    {
+      where: {
+        id: id,
+      },
+      returning: true,
+    }
+  ).then((_menu) =>
+    Menu.findOne({ where: { id: id } }).then((data) =>
+      res.status(200).json(data)
+    )
+  );
+});
+
+controlRouter.put("/kim_hard/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  Menu.update(
+    { kim_hard: "open" },
+    {
+      where: {
+        id: id,
+      },
+      returning: true,
+    }
+  ).then((_menu) =>
+    Menu.findOne({ where: { id: id } }).then((data) =>
+      res.status(200).json(data)
+    )
   );
 });
