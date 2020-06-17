7db9129f71a808571288f5f5ef43b3a0a0e6b735
diosyahrizal <diosyahrizal@gmail.com>
2020-06-17 16:52:21 +0700

    feat(quest): add partial API in quest

--- router/quest.router.ts
+++ router/quest.router.ts
@@ -4,9 +4,8 @@ import { UserSoal } from "../models/users_soal";
 import { Sequelize } from "sequelize";
 import { Soal } from "../models/soals";
 import { sequelize } from "../utils/db";
-import { triggerSoalBadge } from "../utils/checkBadge";
-import { Hasil } from "../models/hasil";
-import { addScore } from "../utils/countScore";
+
+import { addCoin } from "../utils/countScore";
 
 export const questRouter = Router();
 
@@ -21,7 +20,7 @@ questRouter.post("/generate", async (req: Request, res: Response) => {
     if (counter === 0) {
       //Menambahkan soal secara random
       Soal.findAll({
-        attributes: ["id", "matpel", "level"],
+        attributes: ["id", "matpel"],
         where: { matpel: matpel },
         order: [Sequelize.fn("rand")],
         limit: 5,
@@ -86,11 +85,10 @@ questRouter.get("/soal/:id", async (req: Request, res: Response) => {
   res.json({ data: result[0][id] });
 });
 
-questRouter.put("/correction/", async (req: Request, res: Response) => {
-  // FIXME: penyesuaian quest
+questRouter.put("/correction", async (req: Request, res: Response) => {
   const { id, id_soaluser, answer, id_user } = req.body;
 
-  let result;
+  let result: string;
   Soal.findOne({ where: { id: id } }).then((soal) => {
     if (soal && soal.answer === answer) {
       result = "true";
@@ -99,38 +97,11 @@ questRouter.put("/correction/", async (req: Request, res: Response) => {
     }
     UserSoal.update({ result: result }, { where: { id: id_soaluser } })
       .then((_updated) => {
-        triggerSoalBadge(id_user); // TODO: change to trigger Quest
-        res.status(200).json({ status: "updated" });
+        const score = result === "true" ? 600 : 0;
+        addCoin(id_user, score);
+        res.status(200).json({ hasil: result });
       })
       .catch((error) => res.json({ error: error }));
-  });
-});
-
-questRouter.post("/score", async (req: Request, res: Response) => {
-  // FIXME: Penyesuaian score quest
-  const { id_user, matpel } = req.body;
-  let multiple: number;
-
-  UserSoal.findAndCountAll({
-    where: { id_user: id_user, matpel: matpel, result: true },
-  }).then((data) => {
-    const score = data.count * multiple;
-    Hasil.findAndCountAll({
-      where: { id_user: id_user, matpel: matpel },
-    }).then((data) => {
-      if (data.count === 0) {
-        Hasil.create({
-          id_user: id_user,
-
-          matpel: matpel,
-          nilai: score,
-        }).then((_hasil) => res.status(200).json({ score: score }));
-        addScore(id_user, score);
-        // triggerLevel(id_user, level, matpel); //TODO: Trigger point quest?
-      } else {
-        res.status(200).json({ status: "Already submitted" });
-      }
-    });
   });
 });
 
@@ -142,17 +113,15 @@ questRouter.get("/answer", async (req: Request, res: Response) => {
 });
 
 questRouter.get("/progress", async (req: Request, res: Response) => {
-  // FIXME: Add Progress? Hmm
-  const { id_user, matpel } = req.query;
+  // DONE: Add Progress? Hmm
+  const { id_user } = req.query;
 
   const result: any = await sequelize
     .query(
-      `SELECT SUM(IF(result IS NOT NULL AND level = 'Easy' ,1,0)) as easy,
-              SUM(IF(result IS NOT NULL AND level = 'Medium' ,1,0)) as med,
-              SUM(IF(result IS NOT NULL AND level = 'Hard' ,1,0)) as hard
-               FROM user_get_soal WHERE id_user= :id_user AND matpel= :matpel`,
+      `SELECT SUM(IF(result IS NOT NULL AND matpel = 'quest' ,1,0)) as quest,
+               FROM user_get_soal WHERE id_user= :id_user`,
       {
-        replacements: { id_user: id_user, matpel: matpel },
+        replacements: { id_user: id_user },
         logging: console.log,
         plain: false,
         raw: true,
@@ -186,4 +155,9 @@ questRouter.get("/all", async (_req: Request, res: Response) => {
   Soal.findAll({ where: { matpel: "quest" } }).then((soal) => {
     res.status(200).json({ status: 200, data: soal });
   });
+});
+
+questRouter.get("/menu", async (_req: Request, res: Response) => {
+  // TODO: add condition open Quest Menu
+  res.status(200).json({ status: 200, data: "open" });
 });
--- utils/countScore.ts
+++ utils/countScore.ts
@@ -9,3 +9,12 @@ export const addScore = (id_user: string, score: number) => {
     ).then((data) => console.dir(data));
   });
 };
+
+export const addCoin = (id_user: string, score: number) => {
+  User.findOne({ where: { id: id_user } }).then((data) => {
+    const updateCoin = data && data.coin + score;
+    User.update({ coin: updateCoin }, { where: { id: id_user } }).then((data) =>
+      console.dir(data)
+    );
+  });
+};
